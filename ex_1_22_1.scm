25534772c09715a33edd150844df74a2458d749d
verdandi <aimed.fire@gmail.com>
2015-09-11 14:14:07 +0300

    Упражнение 1.22. Дополнение

---
+++ ex_1_22_1.scm
@@ -0,0 +1,47 @@
+(load "ex_1_21.scm")
+
+(define (search-for-primes start-value number-of-primes)
+    (define (next-odd-value x)
+        (if (even? x)
+            (+ x 1)
+            x
+        )
+    )
+
+    (define (search-prime start-value)
+        (define result (timed-prime-test (next-odd-value start-value)))
+        (if (= result 0)
+            (search-prime (+ (next-odd-value start-value) 2))
+            result
+        )
+    )
+
+    (if (> number-of-primes 0)
+        (search-for-primes (+ (search-prime start-value) 1) (- number-of-primes 1))
+    )
+)
+
+(define (timed-prime-test n)
+    (start-prime-test n (runtime))
+)
+
+(define (start-prime-test n start-time)
+    (if (prime? n)
+        (report-prime n (- (runtime) start-time))
+        0
+    )
+)
+
+(define (report-prime n elapsed-time)
+    (display "prime - ")
+    (display n)
+    (display "; ")
+    (display "elapsed time - ")
+    (display (/ elapsed-time internal-time-units-per-second))
+    (newline)
+    n
+)
+
+(define (runtime)
+    (get-internal-run-time)
+)
