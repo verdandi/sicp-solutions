38f30afda4f49a70bbb356cca4902493cf3878a1
verdandi <aimed.fire@gmail.com>
2015-09-17 11:17:37 +0300

    исправлена ошибка в упражнении 2.8

--- chapter2/ex_02_008.scm
+++ chapter2/ex_02_008.scm
@@ -6,8 +6,8 @@
 )
 
 (define (sub-interval i1 i2)
-    (make-interval (- (lower-bound i1) (lower-bound i2))
-                   (- (upper-bound i1) (upper-bound i2)))
+    (make-interval (- (lower-bound i1) (upper-bound i2))
+                   (- (upper-bound i1) (lower-bound i2)))
 )
 
 
