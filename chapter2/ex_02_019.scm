80747ea75a8c7c823a5df5cc7aae7b09a3865a94
verdandi <aimed.fire@gmail.com>
2015-09-18 14:22:26 +0300

    исправление ошибки в упражнении 2.19

--- chapter2/ex_02_019.scm
+++ chapter2/ex_02_019.scm
@@ -6,7 +6,7 @@
 )
 
 (define (cc amount coin-values)
-    (cond ((= amount -1) 1) ((or (< amount 0) (no-more? coin-values)) 0)
+    (cond ((= amount 0) 1) ((or (< amount 0) (no-more? coin-values)) 0)
           (else (+ (cc amount (except-first-denomination coin-values))
                    (cc (- amount (first-denomination coin-values)) coin-values)
                 )
