a3e211dc2367ae549640a9070672b426a86e073a
verdandi <aimed.fire@gmail.com>
2015-09-18 14:15:14 +0300

    упражнение 2.19

---
+++ chapter2/ex_02_019.scm
@@ -0,0 +1,27 @@
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (count-change amount coin-values)
+    (cc amount coin-values)
+)
+
+(define (cc amount coin-values)
+    (cond ((= amount -1) 1) ((or (< amount 0) (no-more? coin-values)) 0)
+          (else (+ (cc amount (except-first-denomination coin-values))
+                   (cc (- amount (first-denomination coin-values)) coin-values)
+                )
+          )
+    )
+)
+
+(define (no-more? coin-values)
+    (null? coin-values)
+)
+
+(define (except-first-denomination coin-values)
+    (cdr coin-values)
+)
+
+(define (first-denomination kind-of-coins)
+    (car kind-of-coins)
+)
